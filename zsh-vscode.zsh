a1e875a10f1b6ecc5c49d14d674b23cec01c6e50
Luis Mayta <luis@hadenlabs.com>
2019-11-22 16:54:06 -0500

    chore: implement functions for zsh-vscode (#9)

--- zsh-vscode.zsh
+++ zsh-vscode.zsh
@@ -8,30 +8,37 @@
 #   Luis Mayta <slovacus@gmail.com>
 #
 
-LIGHT_GREEN='\033[1;32m'
-CLEAR='\033[0m'
+plugin_dir=$(dirname "${0}":A)
+
+# shellcheck source=/dev/null
+source "${plugin_dir}"/src/helpers/messages.zsh
+
+# shellcheck source=/dev/null
+source "${plugin_dir}"/src/helpers/tools.zsh
 
 function vscode::install {
-    echo -e "${CLEAR}${LIGHT_GREEN}Installing Vscode${CLEAR}"
+    message_info "Installing vscode"
     brew cask install visual-studio-code
+    message_success "Installed vscode"
 }
 
 function vscode::init {
     if [ ! -x "$(command which code)" ]; then
-        echo -e "${CLEAR}${LIGHT_GREEN}Installing required vscode packages${CLEAR}"
+        message_error "Is Neccesary Installing required vscode packages"
     fi
 }
 
 function vscode::post_install {
     vscode::load
     if [ -x "$(command which code)" ]; then
-        echo -e "${CLEAR}${LIGHT_GREEN}Installing required vscode packages${CLEAR}"
+        message_info "Installing required vscode packages"
         code --install-extension bierner.markdown-preview-github-styles
         code --install-extension christian-kohler.npm-intellisense
         code --install-extension EditorConfig.EditorConfig
         code --install-extension gornivv.vscode-flutter-files
         code --install-extension ms-azuretools.vscode-docker
 
+        message_info "Installing required vscode packages for languages"
         # languages
         code --install-extension ms-python.python
         code --install-extension ms-vscode.Go
@@ -41,10 +48,11 @@ function vscode::post_install {
         code --install-extension mauve.terraform
         code --install-extension Dart-Code.flutter
 
+        message_info "Installing required vscode packages for git"
         # git
         code --install-extension felipecaputo.git-project-manager
-        code --install-extension waderyan.gitblame
 
+        message_info "Installing required vscode packages for tools"
         # tools
         code --install-extension erd0s.terraform-autocomplete
         code --install-extension hookyqr.beautify
@@ -56,9 +64,11 @@ function vscode::post_install {
         code --install-extension aeschli.vscode-css-formatter
         code --install-extension mgmcdermott.vscode-language-babel
 
+        message_info "Installing required vscode packages for intellisense"
         # intellisense
         code --install-extension christian-kohler.path-intellisense
 
+        message_info "Installing required vscode packages for debug"
         # debug
         code --install-extension msjsdiag.debugger-for-chrome
     fi
