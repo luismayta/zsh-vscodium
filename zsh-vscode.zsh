1c805bc5fbb35666482f604178fb365c49dbbb25
@slovacus <slovacus@gmail.com>
2019-11-24 11:33:24 -0500

    (#11) chore: implement package for vscode

--- zsh-vscode.zsh
+++ zsh-vscode.zsh
@@ -6,7 +6,6 @@
 #
 # Authors:
 #   Luis Mayta <slovacus@gmail.com>
-#
 
 plugin_dir=$(dirname "${0}":A)
 
@@ -36,45 +35,86 @@ function vscode::post_install {
     vscode::load
     if [ -x "$(command which code)" ]; then
         message_info "Installing required vscode packages"
+        code_install aeschli.vscode-css-formatter
         code_install bierner.markdown-preview-github-styles
-        code_install christian-kohler.npm-intellisense
         code_install EditorConfig.EditorConfig
         code_install gornivv.vscode-flutter-files
         code_install ms-azuretools.vscode-docker
+        code_install wix.vscode-import-cost
 
-        message_info "Installing required vscode packages for languages"
         # languages
+        message_info "Installing required vscode packages for languages"
         code_install ms-python.python
         code_install ms-vscode.Go
         code_install trixnz.vscode-lua
-        code_install vscodevim.vim
         code_install Dart-Code.dart-code
         code_install mauve.terraform
         code_install Dart-Code.flutter
 
-        message_info "Installing required vscode packages for git"
         # git
+        message_info "Installing required vscode packages for git"
         code_install felipecaputo.git-project-manager
 
-        message_info "Installing required vscode packages for tools"
         # tools
-        code_install erd0s.terraform-autocomplete
+        message_info "Installing required vscode packages for tools"
         code_install hookyqr.beautify
         code_install CoenraadS.bracket-pair-colorizer
-        code_install christian-kohler.path-intellisense
         code_install mikestead.dotenv
         code_install timonwong.shellcheck
         code_install WakaTime.vscode-wakatime
         code_install aeschli.vscode-css-formatter
         code_install mgmcdermott.vscode-language-babel
+        code_install mgmcdermott.vscode-language-babel
+        code_install formulahendry.auto-rename-tag
+        code_install Gruntfuggly.todo-tree
+        code_instal streetsidesoftware.code-spell-checker
+        code_install bierner.markdown-preview-github-styles
+        code_install dbaeumer.vscode-eslint
+        code_install eg2.vscode-npm-script
 
-        message_info "Installing required vscode packages for intellisense"
         # intellisense
+        message_info "Installing required vscode packages for intellisense"
         code_install christian-kohler.path-intellisense
+        code_install christian-kohler.npm-intellisense
+        code_install leizongmin.node-module-intellisense
+        code_install jasonnutter.search-node-modules
+        code_install erd0s.terraform-autocomplete
 
-        message_info "Installing required vscode packages for debug"
         # debug
+        message_info "Installing required vscode packages for debug"
         code_install msjsdiag.debugger-for-chrome
+
+        # React
+        message_info "Installing required vscode packages for react"
+        code_install jawandarajbir.react-vscode-extension-pack
+        code_install jingkaizhao.vscode-redux-devtools
+        code_install xabikos.ReactSnippets
+
+        # Tools
+        message_info "Installing required vscode packages for tool"
+        code_install esbenp.prettier-vscode
+
+        # TypeScript
+        message_info "Installing required vscode packages for typescript"
+        code_install pmneo.tsimporter
+        code_install rbbit.typescript-hero
+
+        # theme
+        message_info "Installing required vscode packages for Theme"
+        code_install vscode-icons-team.vscode-icons
+
+        # vim
+        message_info "Installing required vscode packages for vim"
+        code_install yatki.vscode-surround
+        code_install vscodevim.vim
+
+        # snippets
+        message_info "Installing required vscode packages for snippets"
+        code_install alexisvt.flutter-snippets
+        code_install dsznajder.es7-react-js-snippets
+        code_install Nash.awesome-flutter-snippets
+        code_install xabikos.JavaScriptSnippets
+
     fi
 }
 
