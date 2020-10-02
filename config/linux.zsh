0cec0b3bafc7fe943234ce4fe15c04e7c837e3e8
@slovacus <slovacus@gmail.com>
2020-10-02 07:20:01 -0500

    chore: implement settings for product (#40)

--- config/linux.zsh
+++ config/linux.zsh
@@ -1,4 +1,5 @@
 #!/usr/bin/env ksh
 # -*- coding: utf-8 -*-
 
-export VSCODIUM_PATH="${HOME}"/.config/VSCodium/User
+export VSCODIUM_PATH="${HOME}/.config/VSCodium/User"
+export VSCODIUM_PRODUCT_FILENAME="/usr/share/vscodium-bin/resources/app/product.json"
