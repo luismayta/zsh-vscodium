778f2b543a7f467d1157cbb57cb1996f01f21fe4
Luis Mayta <luis@hadenlabs.com>
2020-10-02 07:33:18 -0500

    chore: implement install extension (#40)

--- internal/base.zsh
+++ internal/base.zsh
@@ -1,6 +1,6 @@
 #!/usr/bin/env ksh
 # -*- coding: utf-8 -*-
 
-function vscode::internal::extension::install {
+function vscodium::internal::extension::install {
     code --install-extension "${1}" --force
 }
