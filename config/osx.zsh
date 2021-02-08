4c684c2eea944f51ea205ee14d95d8be1e33508c
Luis Mayta <luis@hadenlabs.com>
2021-02-07 21:24:12 -0500

    fix: value vscodium path (#60)

--- config/osx.zsh
+++ config/osx.zsh
@@ -1,5 +1,5 @@
 #!/usr/bin/env ksh
 # -*- coding: utf-8 -*-
 
-export VSCODIUM_PATH="${HOME}/Library/Application\ Support/VSCodium/User"
+export VSCODIUM_PATH=${HOME}/Library/Application\ Support/VSCodium/User
 export VSCODIUM_PRODUCT_FILENAME="/Applications/VSCodium.app/Contents/Resources/app/product.json"
