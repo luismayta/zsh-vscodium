8b2d056c14ac0c1126cbd4317e79060e26bfe28c
@slovacus <slovacus@gmail.com>
2021-02-07 19:39:13 -0500

    refactor: change core to internal (#48)

--- internal/base.zsh
+++ internal/base.zsh
@@ -4,3 +4,28 @@
 function vscodium::internal::extension::install {
     code --install-extension "${1}" --force
 }
+
+function vscodium::internal::vscodium::install {
+    message_info "Installing ${VSCODIUM_PACKAGE_NAME}"
+    message_success "Installed ${VSCODIUM_PACKAGE_NAME}"
+}
+
+function vscodium::internal::vscodium::load {
+    message_warning "Method not implemented for ${VSCODIUM_PACKAGE_NAME}"
+}
+
+function vscodium::internal::curl::install {
+    message_info "Installing curl for ${VSCODIUM_PACKAGE_NAME}"
+    if ! type -p brew > /dev/null; then
+        message_warning "${VSCODIUM_MESSAGE_BREW}"
+    fi
+    brew install curl
+    message_success "Installed curl for ${VSCODIUM_PACKAGE_NAME}"
+}
+
+function vscodium::internal::packages::install {
+    vscodium::internal::packages::fix
+    for package in "${VSCODIUM_PACKAGES[@]}"; do
+       vscodium::internal::extension::install "${package}"
+    done
+}
