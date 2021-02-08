8560efeb623c2c69308f62701b32bd03aae0cf6a
@slovacus <slovacus@gmail.com>
2021-02-07 19:40:15 -0500

    refactor: load zsh core for internal (#48)

--- pkg/linux.zsh
+++ pkg/linux.zsh
@@ -1,16 +1,2 @@
 #!/usr/bin/env ksh
 # -*- coding: utf-8 -*-
-function vscodium::packages::fix {
-    sudo sed -i '' 's,https://open-vsx.org/vscode/gallery,https://marketplace.visualstudio.com/_apis/public/gallery,g' "${VSCODIUM_PRODUCT_FILENAME}"
-    sudo sed -i '' 's,https://open-vsx.org/vscode/item,https://marketplace.visualstudio.com/items,g' "${VSCODIUM_PRODUCT_FILENAME}"
-}
-
-function vscodium::install {
-    message_info "Installing vscodium"
-    if ! type -p brew > /dev/null; then
-        message_warning "VSCODIUM_MESSAGE_BREW"
-        return
-    fi
-    brew cask install vscodium
-    message_success "Installed vscodium"
-}
