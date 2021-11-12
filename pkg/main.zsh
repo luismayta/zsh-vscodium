e59c544fa87495538c30b8d675e8885d1bb49b09
Luis Mayta <luis@hadenlabs.com>
2021-11-12 18:44:04 -0500

    chore: implement core project (#158)

--- pkg/main.zsh
+++ pkg/main.zsh
@@ -16,6 +16,9 @@ function pkg::main::factory {
     esac
     # shellcheck source=/dev/null
     source "${ZSH_VSCODIUM_PATH}"/pkg/helper.zsh
+
+    # shellcheck source=/dev/null
+    source "${ZSH_VSCODIUM_PATH}"/pkg/alias.zsh
 }
 
 pkg::main::factory
