380596d8b0f0010364c900dc87d8017ecb2ac509
Luis Mayta <luis@hadenlabs.com>
2023-09-08 14:24:53 -0300

    chore: implement function for vim on vscodium (#213)

--- internal/osx.zsh
+++ internal/osx.zsh
@@ -15,3 +15,14 @@ function vscodium::internal::vscodium::install {
     brew install --cask vscodium
     message_success "Installed vscodium"
 }
+
+function vscodium::internal::config::vim {
+    message_info "Configuration vim for vscodium"
+    defaults delete -g ApplePressAndHoldEnabled                                           # If necessary, reset global default
+    defaults write com.vscodium ApplePressAndHoldEnabled -bool false
+    defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false              # For VS Code
+    defaults write com.visualstudio.code.oss ApplePressAndHoldEnabled -bool false         # For VS Codium
+    defaults write com.microsoft.VSCodeInsiders ApplePressAndHoldEnabled -bool false      # For VS Code Insider
+    defaults write com.microsoft.VSCodeExploration ApplePressAndHoldEnabled -bool false   # For VS Codium Exploration users
+    message_success "Configuration vim for vscodium"
+}
